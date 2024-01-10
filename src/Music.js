7f55d675f6cfc8fae62ba84b99fc47b8ca85f3b4
Zach <zbayard@gmail.com>
2024-01-10 14:22:07 -0500

    welcome

---
+++ src/Music.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+const Music = ()=> {
+    return (
+      <div className="comp music">
+        <div className="music-row-box">
+            <div className="spotify">spotify</div>
+            <div className="youtube">youtube</div>
+            <div className="soundcloud">soundcloud</div>
+        </div>
+        <div className="socials">
+
+        </div>
+        
+      </div>
+    );
+  }
+  
+  export default Music;
---
+++ src/Welcome.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+const Welcome = ()=> {
+    return (
+      <div className="comp welcome">
+        <div className="welcome-box">
+            <div className="item musics">music</div>
+            <div className="item web-dev">web dev</div>
+            <div className="item ads">ads</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Welcome;
